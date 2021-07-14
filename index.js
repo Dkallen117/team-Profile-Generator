5503e475ac99fb9ced273bc8352e805ca29a2f15
Daniel Allen <danielkentonallen@gmail.com>
2021-07-14 17:30:19 -0600

    finished index.js

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const Intern = require("./lib/intern");
 
 let assembledTeamArray = [];
 
+function startProjectName() {
+    inquirer.prompt([
+        {
+            message: "/////////Time to Generate Your Team! Please write your the name of your project://///////",
+            name: "projectname"
+        }
+    ])
+        .then(function(data){
+            const projectName = data.projectname
+            assembledTeamArray.push(projectName)
+            addManager();
+        })
+
+    
+}
+
 function addManager() {
 
     inquirer.prompt([
@@ -76,7 +92,7 @@ function additionalTeamMems(){
             break;
 
         case "My team is complete":
-            assembleTeam();
+            finalTeam();
             break;
 
 
@@ -147,7 +163,7 @@ function addIntern(){
     },
     {
 
-        message: "Whta is the intern's school?",
+        message: "What is the intern's school?",
         name: "school"
 
     }
@@ -169,3 +185,125 @@ function addIntern(){
     ])
 
 }
+
+function finalTeam(){
+
+    const htmlSectionsArray = []
+    const htmlTop = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>${assembledTeamArray[0]}</title>
+    <!-- Font Awesome -->
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+    />
+    <!-- Google Fonts -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
+    <!-- Bootstrap core CSS -->
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <!-- Material Design Bootstrap -->
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css"
+      rel="stylesheet"
+    />
+    
+</head>
+
+<body>
+<header>
+
+  
+    
+<!-- Container for header  -->
+<div class="row">
+
+<h1>${assembledTeamArray[0]}</h1>
+
+
+</div>
+
+</header>
+
+<main class="container">
+
+
+  <div class="row">
+  
+  <div class="row row-cols-1 row-cols-md-2">
+  
+  <div class="col-md-4 col-sm-12 mb-4">`
+
+  htmlSectionArrays.push(htmlTop);
+
+  for (let i =1; i < assembledTeamArray.length; i++){
+
+
+    let newEmployee = `<div class="card">
+    <div class="card-body">
+      <h5 class="card-title">${assembledTeamArray[i].name}</h5>
+      <p class="card-text">
+        <ul>
+        <li>${assembledTeamArray[i].title}</li>
+        <li>Employee ID: ${assembledTeamArray[i].id}</li>
+        <li>Email: <a href="mailto:${assembledTeamArray[i].email}">${assembledTeamArray[i].email}</a>></li>
+      `
+      if (assembledTeamArray[i].officeNum) {
+        object += `
+        <li>${assembledTeamArray[i].officeNum}</li>
+        `
+        }
+
+      if (assembledTeamArray[i].github) {
+        newEmployee += `
+        <li>GitHub: <a href="https://github.com/${assembledTeamArray[i].github}">${assembledTeamArray[i].github}</a></li>
+            `
+        }
+
+      if (finalTeamArray[i].school) {
+        newEmployee += `
+        <li>School: ${assembledTeamArray[i].school}</li>
+            `
+        }
+
+    newEmployee +=`
+        </ul>
+        </p>
+    </div>
+    </div>
+    
+    `
+    htmlSectionsArray.push(newEmployee);
+
+  }
+
+  const htmlBottom = `
+  </div>
+  </div>
+  </div>
+  </main>
+  </body>
+  </html>
+  `
+  htmlSectionsArray.push(htmlBottom);
+
+  fs.writeFile(`./generated-html/${assembledTeamArray[0]}.html`, htmlSectionsArray.join(""), function (err) {
+      
+  })
+}
+
+startProjectName();
+
+
+
+
